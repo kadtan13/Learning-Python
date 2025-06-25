de4ad0009b064656753ec2e68f5a72da342d82bb
Mercury Reaper <tanishqk3011@gmail.com>
2025-06-25 17:21:34 +0530

    variables.py

---
+++ variables.py
@@ -0,0 +1,20 @@
+a = complex(1, 2)
+b = True
+c = "Tanishq"
+d = None
+a1 = 9
+print(a)
+print(b)
+print(c)
+print(d)
+print(a + a1)
+print("The type of a is ", type(a))
+print("The type of b is ", type(b))
+print("The type of c is ", type(c))
+print("The type of d is ", type(d))
+list1 = [13, 7.1, [-1, 2], ["Pineapple", "Kiwi"]]
+print(list1)
+tuple1 = (("Pigeon", "Crow"), ("Messi", "Yamal"))
+print(tuple1)
+dict1 = {"Name":"Stephen Curry", "age":37, "hasRing?":True}
+print(dict1)
