ccf2eae0376a3011a3b8c4b48fd503ab1ecb1ff4
Mercury Reaper <tanishqk3011@gmail.com>
2025-06-25 18:37:06 +0530

    poem.py

--- poem.py
+++ poem.py
@@ -1,5 +1,8 @@
 print ("Time and the bell have buried the day,\nThe black cloud carries the sun away")
-print ('''The day is done, and the "darkness"\nGrows with the night, and the night\nWith the darkness, and the darkness\nWith the day, and the day.\nThe days' lights fades aways as the darkness grows''')
+print ('''The day is done, and the "darkness"
+Grows with the night, and the night
+With the darkness, and the darkness\nWith the day, and the day.
+The days' lights fades aways as the darkness grows''')
 print ("I am 'Legend', and I will never surrender to the \n\"Darkness\"")
 print ("\nBy: Tanishq", 2025, "IPMO6195", "IIM Rohtak", sep=" ~ ", end="\n13\n")
 print ("Copyright License No:", 1234, 8765)
